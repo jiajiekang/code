f7db6012f451a46e3f5e24608f04141e1ddfad67
Jiajie Kang <jiajie.kang@dtone.com>
2023-01-07 18:20:29 +0800

    Reference Ex Day 1

--- try.pl
+++ try.pl
@@ -1,4 +1,7 @@
 use Data::Dump qw(dump);
+
+$sepline = join '', "\n", "-" x 6, "\n";
+
 $record = {
     NAME => "Jason",
     EMPNO => 132,
@@ -8,8 +11,7 @@ $record = {
     PALS => [ "Norbert", "Rhys", "Phineas"],
 };
 printf "I am %s, and my pals are %s.\n",
-$record->{NAME},
-join(", ", @{$record->{PALS}});
+$record->{NAME}, join(", ", @{$record->{PALS}});
 
 $byname{ $record->{NAME} } = $record;
 
@@ -20,12 +22,17 @@ if ($rp = $byname{"Aron"}) {
 push @{ $byname{"Jason"}->{PALS} }, "Theodore";
 printf "Jason now has %d pals\n", scalar @{$byname{"Jason"}->{PALS}};
 
-print dump $record;
-
-while (($name, $record) = each %byname) {
-    printf "%s is employee number %d\n", $name, $record->{EMPNO};
+{ local $record;
+    while (($name, $record) = each %byname) {
+        printf "%s is employee number %d\n", $name, $record->{EMPNO};
+    }
 }
 
-print "\n---\n";
-print dump \%byname;;
+printf "%s\n", dump \%byname;
+print $sepline;
 
+$employees[ $record->{EMPNO} ] = $record;
+
+if ($rp = $employees[132]) {
+    printf "employee number 132 is %s\n", $rp->{NAME};
+}
