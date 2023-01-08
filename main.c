aaf2cf11db1881b4f8876811d1d69e2d9bc28c2c
Jiajie Kang <jiajie.kang@dtone.com>
2023-01-08 20:30:59 +0800

    simulate asm record

--- main.c
+++ main.c
@@ -1,12 +1,26 @@
-#include <stido.h>
+#include <stdio.h>
+
+struct company {
+    char cn[3];
+    char hn[9];
+    short pm;
+    short sr;
+    char cp[3];
+};
+
+struct company dec = {"DEC", "Ken Olsen", 137, 40, "PDP"};
 
 int main() {
-    char a[][16] = { "ibm             ", "dec             ", "dos             ", "vax             " };
-    for (int i = 0; i < sizeof a / sizeof a[0]; i++) {
-        for (int j = 0; j < 3; j++) {
-            a[i][j] &= 0b11011111;
-        }
-        printf("%s\n", a[i][j]);
-    }
+    int i;
+    dec.pm = 38;
+    dec.sr += 70;
+    
+    i = 0;
+    dec.cp[i] = 'V';
+    i++;
+    dec.cp[i] = 'A';
+    i++;
+    dec.cp[i] = 'A';
+
     return 0;
 }
