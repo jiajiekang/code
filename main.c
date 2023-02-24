5d84a8987964926074c1160dbd0b9239f2b9234a
Jiajie Kang <jiajie.kang@dtone.com>
2023-02-24 17:28:56 +0800

    Ex1_24

--- main.c
+++ main.c
@@ -5,34 +5,25 @@
 #define TRUE (1 == 1)
 #define FALSE !TRUE
 
-// This is a test comment.
+int get_str(char str[], int limit);
+void check_syntax(char str[]);
 
-int get_str(char str[], int limit); // This is another test comment.
-void remove_comments(char str[], char no_com_str[]);
-
-int main(void) {
-  /**
-   * This is multiline
-   * block
-   * comment.
-   */
-
+int main(void)
+{
   char str[MAXSTR];
-  char no_com_str[MAXSTR];
 
   get_str(str, MAXSTR);
-
-  remove_comments(str, no_com_str);
-
-  printf("%s", no_com_str);
+  check_syntax(str);
 
   return 0;
 }
 
-int get_str(char str[], int limit) {
+int get_str(char str[], int limit)
+{
   int c, i = 0;
 
-  while (i < limit - 1 && (c = getchar()) != EOF) {
+  while (i < limit - 1 && (c = getchar()) != EOF)
+  {
     str[i++] = c;
   }
   str[i] = '\0';
@@ -40,50 +31,124 @@ int get_str(char str[], int limit) {
   return i;
 }
 
-void remove_comments(char str[], char no_com_str[]) {
-  int in_quote = FALSE;
-  int line_comment = FALSE;
-  int block_comment = FALSE;
+void check_syntax(char str[])
+{
+  int parentheses = 0;
+  int brackets = 0;
+  int braces = 0;
 
-  int i = 0, j = 0;
-  while (str[i] != '\0') {
-    if (!block_comment) {
-      if (!in_quote && str[i] == '"') {
-        in_quote = TRUE;
-      } else if (in_quote && str[i] == '"') {
-        in_quote = FALSE;
+  int single_quotes = FALSE;
+  int double_quotes = FALSE;
+
+  int block_comment = FALSE;
+  int line_comment = FALSE;
+
+  int i = 0;
+  while (str[i] != '\0' && parentheses >= 0 && brackets >= 0 && braces >= 0)
+  {
+    if (!line_comment && !block_comment && !single_quotes && !double_quotes)
+    {
+      if (str[i] == '(')
+      {
+        ++parentheses;
+      }
+      else if (str[i] == ')')
+      {
+        --parentheses;
+      }
+
+      if (str[i] == '[')
+      {
+        ++brackets;
+      }
+      else if (str[i] == ']')
+      {
+        --brackets;
+      }
+
+      if (str[i] == '{')
+      {
+        ++braces;
+      }
+      else if (str[i] == '}')
+      {
+        --braces;
       }
     }
 
-    if (!in_quote) {
-      if (str[i] == '/' && str[i + 1] == '*' && !line_comment) {
+    if (!line_comment && !block_comment)
+    {
+      if (str[i] == '\'' && !single_quotes && !double_quotes)
+      {
+        single_quotes = TRUE;
+      }
+      else if (single_quotes && str[i] == '\'' && (str[i - 1] != '\\' || str[i - 2] == '\\'))
+      {
+        single_quotes = FALSE;
+      }
+
+      if (str[i] == '"' && !single_quotes && !double_quotes)
+      {
+        double_quotes = TRUE;
+      }
+      else if (double_quotes && str[i] == '"' && (str[i - 1] != '\\' || str[i - 2] == '\\'))
+      {
+        double_quotes = FALSE;
+      }
+    }
+
+    if (!single_quotes && !double_quotes)
+    {
+      if (str[i] == '/' && str[i + 1] == '*' && !line_comment)
+      {
         block_comment = TRUE;
       }
-
-      if (str[i] == '*' && str[i + 1] == '/') {
+      else if (str[i] == '*' && str[i + 1] == '/')
+      {
         block_comment = FALSE;
-        i += 2;
       }
 
-      if (str[i] == '/' && str[i + 1] == '/') {
+      if (str[i] == '/' && str[i + 1] == '/' && !block_comment)
+      {
         line_comment = TRUE;
       }
-
-      if (str[i] == '\n') {
+      else if (str[i] == '\n')
+      {
         line_comment = FALSE;
       }
-
-      if (line_comment || block_comment) {
-        ++i;
-      } else if (!line_comment || !block_comment) {
-        no_com_str[j++] = str[i++];
-      }
-    } else {
-      no_com_str[j++] = str[i++];
     }
+
+    ++i;
   }
 
-  no_com_str[j] = '\0';
+  if (parentheses)
+  {
+    printf("Error: unbalanced parentheses.\n");
+  }
+
+  if (brackets)
+  {
+    printf("Error: unbalanced brackets.\n");
+  }
+
+  if (braces)
+  {
+    printf("Error: unbalanced braces.\n");
+  }
+
+  if (single_quotes)
+  {
+    printf("Error: unbalanced single quotes.\n");
+  }
+
+  if (double_quotes)
+  {
+    printf("Error: unbalanced double quotes.\n");
+  }
+
+  if (block_comment)
+  {
+    printf("Error: block comment not closed.\n");
+  }
 }
 
-// NOTE: run: ./c_remove_comments < c_remove_comments.c
