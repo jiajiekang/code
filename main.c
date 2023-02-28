42610f264fe1446e9b9fec032eae7cf1af19c70f
Jiajie Kang <jiajie.kang@dtone.com>
2023-02-28 16:40:13 +0800

    Ex2_06

--- main.c
+++ main.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-#define MAXSTR 1000
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c\n"
+#define BYTE_TO_BINARY(byte)                                                   \
+  (byte & 128 ? '1' : '0'), (byte & 64 ? '1' : '0'), (byte & 32 ? '1' : '0'),  \
+      (byte & 16 ? '1' : '0'), (byte & 8 ? '1' : '0'), (byte & 4 ? '1' : '0'), \
+      (byte & 2 ? '1' : '0'), (byte & 1 ? '1' : '0')
 
-int any(char str1[], char str2[]);
+unsigned int setbits(int x, int p, int n, int y);
 
 int main(void) {
-  char str1[MAXSTR] = "xxxabcabc";
-  char str2[MAXSTR] = "cbaa";
+  unsigned int x = 0b11111111;
+  unsigned int y = 0b0110;
 
-  printf("%d", any(str1, str2));
+  printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(x));
+  printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(setbits(x, 2, 4, y)));
 
   return 0;
 }
 
-int any(char str1[], char str2[]) {
-  int i, j;
-  for (i = 0; str1[i] != '\0'; ++i) {
-    for (j = 0; str2[j] != '\0'; ++j) {
-      if (str1[i] == str2[j]) {
-        return i;
-      }
-    }
-  }
+unsigned int setbits(int x, int p, int n, int y) {
+  ++p; // First position is 0
 
-  return -1;
+  unsigned int mask1 = (~(~(~0 << n) << p) & x);
+  unsigned int mask2 = (~(~0 << n) & y) << p;
+
+  return mask1 | mask2;
 }
 
-// NOTE: The standard library (string.h), cotains the function strpbrk which
-// returns a pointer to the location of the char from the first string.
+// NOTE: Masking is a very good technique to work with bits. We can think about
+// logic AND as a multiply and for OR as an addition.
--- try.pl
+++ try.pl
@@ -45,3 +45,31 @@ use utf8;
 #   print "$food is $food_color{$food}.\n";
 # }
 
+# use Tie::IxHash;
+# tie %food_color, "Tie::IxHash";
+# $food_color{"Banana"} = "Yellow";
+# $food_color{"Apple"}  = "Green";
+# $food_color{"Lemon"}  = "Yellow";
+# print "In insertion order, the foods are:\n";
+# foreach $food ( keys %food_color ) {
+#   print " $food\n";
+# }
+# print "Still in insertion order, the foods' colors are:\n";
+# while ( ( $food, $color ) = each %food_color ) {
+#   print "$food is colored $color.\n";
+# }
+
+# %ttys = ();
+# open( WHO, "who|" ) or die "can't open who: $!";
+# while (<WHO>) {
+#   ( $user, $tty ) = split;
+#   push( @{ $ttys{$user} }, $tty );
+# }
+# foreach $user ( sort keys %ttys ) {
+#   print "$user: @{$ttys{$user}}\n";
+# }
+
+# %surname    = ( "Mickey" => "Mantle", "Babe" => "Ruth" );
+# %first_name = reverse %surname;
+# print $first_name{"Mantle"}, "\n";
+
