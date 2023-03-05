181883e81f4149751f6add0aac8083ec635dd8de
Jiajie Kang <jiajie.kang@dtone.com>
2023-03-05 16:01:10 +0800

    Ex3_06

--- main.c
+++ main.c
@@ -1,64 +1,67 @@
-#include <ctype.h>
+#include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define MAXLEN 10000
+#define MAXLEN 1000
 
-int get_str(char str[], int limit);
-void expand(char src[], char dest[]);
+int ilen(int a);
+void int_to_array(int n, char s[], int padding);
+void str_reverse(char str1[], int index, int size);
 
 int main(void) {
-  char str[MAXLEN];
-  char expanded_str[MAXLEN];
+  int n = 1995;
+  char number_str[MAXLEN];
 
-  get_str(str, MAXLEN);
-  expand(str, expanded_str);
-  printf("%s", expanded_str);
+  int_to_array(n, number_str, 6);
+  printf("%s\n", number_str);
 
   return 0;
 }
 
-int get_str(char str[], int limit) {
-  int c, i = 0;
+int ilen(int a) {
+  int i = 0;
 
-  while (i < limit - 1 && (c = getchar()) != EOF) {
-    str[i++] = c;
-  }
-  str[i] = '\0';
+  do {
+    ++i;
+  } while (a /= 10);
 
   return i;
 }
 
-void expand(char src[], char dest[]) {
-  /**
-   * a-z
-   * a-b-c
-   * a-c-h-v
-   * a-c-b-v
-   * 0-9
-   * 1-5
-   * a-zA-Z
-   * 0-9a-zA-Z
-   * -a-z
-   * a-z-
-   * -a-z-
-   */
-  int i, j = 0;
-  for (i = 0; i < MAXLEN - 1 && j < MAXLEN - 1 && src[i] != EOF; ++i) {
-    if (isalnum(src[i]) && src[i + 1] == '-' && src[i] < src[i + 2]) {
-      do {
-        int k;
-        for (k = 0; k <= (src[i + 2] - src[i]); ++k) {
-          int temp = src[i] + k;
-          if (dest[j - 1] != temp && (isdigit(temp) || isalpha(temp))) {
-            dest[j++] = temp;
-          }
-        }
+void int_to_array(int n, char s[], int padding) {
+  int i = 0, sign = n;
 
-        i += 2;
-      } while (isalnum(src[i]) && src[i + 1] == '-' && src[i] < src[i + 2]);
-    } else {
-      dest[j++] = src[i];
-    }
+  do {
+    s[i++] = abs(n % 10) + '0';
+  } while (n /= 10);
+
+  if (sign < 0) {
+    s[i++] = '-';
   }
-  dest[j] = '\0';
+
+  int len = ilen(sign);
+  while (len < padding) {
+    s[i++] = ' ';
+    -- padding;
+  }
+
+  s[i] = '\0';
+
+  int s_len = strlen(s);
+  str_reverse(s, 0, s_len - 1);
+}
+
+void str_reverse(char str1[], int index, int size) {
+  char temp;
+
+  temp = str1[index];
+  str1[index] = str1[size - index];
+  str1[size - index] = temp;
+
+  if (index == size / 2) {
+    return;
+  }
+
+  str_reverse(str1, index + 1, size);
 }
