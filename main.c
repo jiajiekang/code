23f611c3a240ab408242ef4e37815e4262cd062c
Jiajie Kang <jiajie.kang@dtone.com>
2023-03-08 12:22:58 +0800

    Ex4_11

--- main.c
+++ main.c
@@ -1,39 +1,15 @@
 #include <stdio.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <math.h>
 
 #define MAXOP 100
 #define MAXVAL 100
 #define BUFFSIZE 100
-#define MAXLEN 1000
 #define NUMBER '0'
 
-#define VARSET '_'
-#define VARGET 'a'
-#define VARNUM 26
-
-enum boolean
-{
-  FALSE,
-  TRUE
-};
-
-int get_line(char line[], unsigned int max_line_len);
 int getop(char[]);
-
-void push(double f);
+void push(double);
 double pop(void);
-int is_empty(void);
-void view_head(void);
-void duplicate(void);
-void swap(void);
-void clear(void);
-
-char var = '0';
-char line[MAXLEN];
-int line_i = 0;
 
 int main(void)
 {
@@ -41,109 +17,61 @@ int main(void)
   double op2;
   char s[MAXOP];
 
-  int varindex = 0;
-  double var_buff[VARNUM];
-
-  while (get_line(line, MAXLEN) != 0)
+  while ((type = getop(s)) != EOF)
   {
-    line_i = 0;
-    while ((type = getop(s)) != '\0')
+    switch (type)
     {
-      switch (type)
+    case NUMBER:
+      push(atof(s));
+      break;
+
+    case '+':
+      push(pop() + pop());
+      break;
+
+    case '-':
+      op2 = pop();
+      push(pop() - op2);
+      break;
+
+    case '*':
+      push(pop() * pop());
+      break;
+
+    case '/':
+      op2 = pop();
+
+      if (op2 != 0.0)
       {
-      case NUMBER:
-        push(atof(s));
-        break;
-
-      case '+':
-        push(pop() + pop());
-        break;
-
-      case '-':
-        op2 = pop();
-        push(pop() - op2);
-        break;
-
-      case '*':
-        push(pop() * pop());
-        break;
-
-      case '/':
-        op2 = pop();
-
-        if (op2 != 0.0)
-        {
-          push(pop() / op2);
-        }
-        else
-        {
-          printf("Error: zero divisor.\n");
-        }
-
-        break;
-
-      case '%':
-        op2 = pop();
-
-        if (op2 != 0.0)
-        {
-          push((int)pop() % (int)op2);
-        }
-        else
-        {
-          printf("Error: zero divisor.\n");
-        }
-        break;
-
-      case '^':
-        op2 = pop();
-        push(pow(pop(), op2));
-        break;
-
-      case '~':
-        push(sin(pop()));
-        break;
-
-      case 'e':
-        push(exp(pop()));
-        break;
-
-      case 'h':
-        view_head();
-        break;
-
-      case 'd':
-        duplicate();
-        break;
-
-      case 's':
-        swap();
-        break;
-
-      case 'c':
-        clear();
-        break;
-
-      case VARSET:
-        var_buff[varindex++] = pop();
-        printf("variable %c: %.3f\n", 'a' + varindex - 1, var_buff[varindex - 1]);
-        break;
-
-      case VARGET:
-        push(var_buff[var - 'a']);
-        break;
-
-      case '\n':
-        if (!is_empty())
-        {
-          printf("result: %.8g\n", pop());
-        }
-        break;
-
-      default:
-        printf("Error: unknown command %s.\n", s);
-        break;
+        push(pop() / op2);
       }
+      else
+      {
+        printf("Error: zero divisor.\n");
+      }
+
+      break;
+
+    case '%':
+      op2 = pop();
+
+      if (op2 != 0.0)
+      {
+        push((int)pop() % (int)op2);
+      }
+      else
+      {
+        printf("Error: zero divisor.\n");
+      }
+      break;
+
+    case '\n':
+      printf("result: %.8g\n", pop());
+      break;
+
+    default:
+      printf("Error: unknown command %s.\n", s);
+      break;
     }
   }
 
@@ -151,17 +79,17 @@ int main(void)
 }
 
 int sp = 0;
-double stack[MAXVAL];
+double val[MAXVAL];
 
 void push(double f)
 {
   if (sp < MAXVAL)
   {
-    stack[sp++] = f;
+    val[sp++] = f;
   }
   else
   {
-    printf("Error: stack full.\n");
+    printf("Error: stack full, can't push %g.\n", f);
   }
 }
 
@@ -169,96 +97,24 @@ double pop(void)
 {
   if (sp > 0)
   {
-    return stack[--sp];
+    return val[--sp];
   }
   else
   {
     printf("Error: stack empty.\n");
+    return 0.0;
   }
-
-  return 0.0;
-}
-
-int is_empty(void)
-{
-  if (sp > 0)
-  {
-    return FALSE;
-  }
-
-  return TRUE;
-}
-
-void view_head(void)
-{
-  if (sp)
-  {
-    printf("stack_head: %g\n", stack[sp - 1]);
-  }
-  else
-  {
-    printf("Error: stack empty.\n");
-  }
-}
-
-void duplicate(void)
-{
-  double temp = pop();
-  push(temp);
-  push(temp);
-}
-
-void swap(void)
-{
-  double temp1 = pop();
-  double temp2 = pop();
-
-  push(temp1);
-  push(temp2);
-}
-
-void clear(void)
-{
-  do
-  {
-    stack[sp] = 0.0;
-  } while (sp--);
-}
-
-int get_line(char line[], unsigned int max_line_len)
-{
-  int c, i;
-
-  for (i = 0; i < max_line_len - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
-  {
-    line[i] = c;
-  }
-
-  if (c == '\n')
-  {
-    line[i] = c;
-    ++i;
-  }
-
-  line[i] = '\0';
-
-  return i;
 }
 
 int getop(char s[])
 {
   int i = 0, c;
+  static int buf = EOF;
 
-  while ((s[0] = c = line[line_i++]) == ' ' || c == '\t')
+  while ((s[0] = c = getchar()) == ' ' || c == '\t')
     ;
 
   s[1] = '\0';
-
-  if (isalpha(c))
-  {
-    var = c;
-    return VARGET;
-  }
 
   if (!isdigit(c) && c != '.' && c != '-')
   {
@@ -267,34 +123,36 @@ int getop(char s[])
 
   if (c == '-')
   {
-    int next = line[line_i++];
+    int next = getchar();
     if (!isdigit(next) && next != '.')
     {
       return next;
     }
 
     s[i] = c;
-    c = next;
-    --line_i;
+    c = next = buf;
   }
   else
   {
-    c = line[line_i++];
+    c = getchar();
   }
 
   if (isdigit(c))
   {
-    while (isdigit(s[++i] = c = line[line_i++]))
+    while (isdigit(s[++i] = c = getchar()))
       ;
   }
 
   if (c == '.')
   {
-    while (isdigit(s[++i] = c = line[line_i++]))
+    while (isdigit(s[++i] = c = getchar()))
       ;
   }
 
-  --line_i;
+  if (c != EOF)
+  {
+    buf = c;
+  }
 
   return NUMBER;
 }
--- try.pl
+++ try.pl
@@ -165,3 +165,10 @@ use utf8;
 # print "@a\n";
 # say "@b";
 
+$a = 'nobody';
+$b = 'bodysnatcher';
+if ( "$a $b" =~ /^(\w+)(\w+) \2(\w+)$/ ) {
+  print "$2 overlaps in $1-$2-$3\n";
+}
+
+
